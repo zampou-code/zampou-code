e32aa759104b457926ff797eca84fa971cd94772
Zampou Ibrahim <zampou@192.168.1.25>
2024-03-12 15:57:54 +0000

    first commit

---
+++ readme.js
@@ -0,0 +1,66 @@
+module.exports = `
+<section align="center">
+    <img src="https://readme-typing-svg.herokuapp.com/?font=Righteous&size=35&center=true&vCenter=true&width=500&height=70&duration=4000&color=38A1E6&lines=Hi+There!+👋;+I'm+Zi+Code!;"/>
+</section>
+
+![MasterHead](https://i0.wp.com/wanderin.dev/wp-content/uploads/2019/12/crop-0-0-1170-390-0-about-cover.png?fit=1170%2C390&ssl=1)
+
+<section align="center"> 
+  <h2 align="center">A passionate web 🧑‍💻 artisan 🧶 from Ivory Coat 🇨🇮</h2>
+  <a href="#">
+    <img src="https://img.shields.io/badge/X (Twitter)-333333?style=for-the-badge&logo=x&logoColor=red" />
+  </a>
+  <a href="https://linkedin.com/in/zi-code" target="_blank">
+    <img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" target="_blank" />
+  </a>
+  <a href="#" target="_blank">
+     <img src="https://img.shields.io/badge/Portfolio-FF5722?style=for-the-badge&logo=google-chrome&logoColor=white" target="_blank" />
+  </a>
+</section>
+
+<section>
+    <h2 align="center">Working Tools 🧰</h2>
+    <div align="left">
+        <h3 >Font-end</h3>
+        <img src="https://skillicons.dev/icons?i=nextjs,nuxt,tailwind,materialui" />
+    </div> 
+    <div align="center">
+        <h3>Back-end</h3>
+        <img src="https://skillicons.dev/icons?i=laravel,adonis,expressjs,nodejs" />
+    </div >
+    <div align="right" >
+        <h3>CI/CD</h3>
+        <img src="https://skillicons.dev/icons?i=git,github,docker,githubactions" />
+    </div>
+    <br />
+    <p align="center">I like to use this technology to create new products with my own hands ✋ like a artisan</p>
+</section>
+
+<section align="center">
+    <h2 align="center">Contributions 🤝</h2>
+    <img alt="snake eating my contributions" src="https://raw.githubusercontent.com/zampou-code/zampou-code/output/github-contribution-grid-snake.svg" />
+</section>
+
+<section align="center">
+    <h2 align="center">statistic 📊</h2>
+    <img width=377 src="https://github-readme-stats.vercel.app/api?username=zampou-code&show_icons=true&locale=en&theme=tokyonight" alt="zampou-code" />
+    <img width=400 src="https://github-readme-streak-stats.herokuapp.com/?user=rishavchanda&&theme=tokyonight" alt="zampou-code" />
+    <br />
+    <img width=325 align="center" src="https://github-readme-stats.vercel.app/api/top-langs?username=zampou-code&show_icons=true&locale=en&layout=compact&theme=tokyonight" alt="zampou-code" />
+</section>
+
+<section align="center">
+    <h2 align="center">Visitor count 📊</h2>
+    <img src="https://profile-counter.glitch.me/zampou-code/count.svg" />
+    <p>Added the \`Thu 13 Mar 2024\`</p>
+</section>
+
+<section align="center">
+<hr />
+Last update on <#today_date>
+
+<#day_before_new_years> days before new years
+
+<#zbot_signing>
+</section>
+`;
